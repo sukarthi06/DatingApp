0b978afd876a32ab466243de824370b4471c104b
Karthikeya Sundaramoorthy <sukarthilenovo@outlook.com>
2025-12-31 23:37:01 -0500

    Router changes

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,51 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
 
 @Component({
-  selector: 'app-root',
-  //imports: [RouterOutlet],
+  selector: 'app-root',  
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Nav, Home]
+  imports: [Nav, RouterOutlet]
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = 'Dating App';//signal('Dating App');
-  protected members = signal<User[]>([]);
+export class App {
+  
+  protected router = inject(Router);
   
   constructor(){}
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-
-    // this.http.get('http://localhost:5126/api/members').subscribe({
+  // this.http.get('http://localhost:5126/api/members').subscribe({
     //   next: response => this.members.set(response),
     //   error: error => console.log(error),
     //   complete: () => console.log('Completed the http request')
     // });
-  }
-
-  setCurrentUser(){
-    const userString = localStorage.getItem('DAppUser');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers(){
-    try {
-      return lastValueFrom(this.http.get<User[]>('http://localhost:5126/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
